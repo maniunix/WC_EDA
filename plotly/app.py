bc90dc9893ce58868feab026d7bdc8c5035001e3
maniunix <manishpadole13@gmail.com>
2023-08-08 19:25:41 +0530

    CSS change

--- plotly/app.py
+++ plotly/app.py
@@ -22,20 +22,25 @@ app.layout = html.Div([
         id='item-dropdown',
         style={
             "position": "absolute",
-            "left": "200px",
-            "width": "300px"
+            "left": "500px",
+            "width": "200px"
         },
         value="Germany"  # Default value for dropdown
     ),
     dcc.Dropdown(
         id="category-dropdown",
         options=[{'label': yr, 'value': yr} for yr in year],
-        style={"width": "300px"},
+        style={"width": "200px",
+               "left":"100px"},
         value="1934"  # Default value for dropdown
     ),
-    html.Div(id='output-div'),
-    dcc.Graph(id="graph")
+    html.Div(id='output-div', style= {"text-align": "center"}),
+    dcc.Graph(id="graph"),
+    html.Div(html.P(["This Dashboard contains all the countries which participated in Football Worldcup thoughout the Years.",html.Br(), "The Graph shows the Win and Loss percentage of country participated."],
+           style= {"text-align": "center"}))
 ])
+
+
 
 @app.callback(
     dash.dependencies.Output('item-dropdown', 'options'),
@@ -49,12 +54,12 @@ def update_dropdown(value):
     value = df['Team'].iloc[0]
     return options, value
 
-@app.callback(
-    dash.dependencies.Output('output-div', 'children'),
-    [dash.dependencies.Input('item-dropdown', 'value')]
-)
-def update_output(selected_item):
-    return f'You selected: {selected_item}'
+# @app.callback(
+#     dash.dependencies.Output('output-div', 'children'),
+#     [dash.dependencies.Input('item-dropdown', 'value')]
+# )
+# def update_output(selected_item):
+#     return f'You selected: {selected_item}'
 
 
 @app.callback(
@@ -69,10 +74,15 @@ def update_graph(selected_team):
     win_percentage = filtered_df['WinPercentage'].iloc[0]
     loss_percentage = filtered_df['LossPercentage'].iloc[0]
     
-    fig = px.pie(names=['Win', 'Loss'], values=[win_percentage, loss_percentage],
-                 title=f'Win-Loss Percentage for {selected_team}', hole=0.3)
+    # fig = px.pie(names=['Win', 'Loss'], values=[win_percentage, loss_percentage],
+    #              title=f'Win-Loss Percentage for {selected_team}', hole=0.3),
+    # fig.update_layout(title_x=0.5)
+    fig = go.Figure(data=[go.Pie(labels=['Win', 'Loss'], values=[win_percentage, loss_percentage])])
+    # fig = go.Figure(fig)
+    
+    fig.update_layout(title={'text': f'Win-Loss Percentage for {selected_team}', 'x': 0.5})
     return fig
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host =" 192.168.0.1",port = 8050, debug=True)
